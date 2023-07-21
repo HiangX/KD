93d57e37b386478e3571ad3ecc70d7c7d6c095de
HiangXu <hiangxu@HiangXudeMacBook-Pro.local>
2023-07-21 09:51:09 +0800

    rm init

--- models/linear.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-class WeightLayer(nn.Module):
-    def __init__(self, input_dim):
-        nn.Module.__init__(self)
-        self.main = nn.Linear(input_dim, 1)
-    def forward(self, x):
-        return self.main(x)
